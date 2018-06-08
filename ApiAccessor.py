e3ec740dc5a78e330d55e9cacf199d76c3131735
thechucklingatom <thechucklingatom>
2018-06-07 21:38:41 -0600

    start of the ui, and the document creator done.

--- ApiAccessor.py
+++ ApiAccessor.py
@@ -1,7 +1,31 @@
 from mtgsdk import Set
 from mtgsdk import Card
 import DocumentWriter
+import tkinter
+import ttk
 
+def name_list(setList):
+    return_list = []
+    for mtgSet in setList:
+        return_list.append(mtgSet.name)
+    return_list.sort()
+    return return_list
+
+class Application(tkinter.Frame):
+
+    def __init__(self, master=tkinter.Tk()):
+        master.geometry("500x500")
+        tkinter.Frame.__init__(self, master, width=400, height=400)
+        self.pack_propagate(0)
+        self.grid()
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.setList = ttk.Combobox(self, textvariable="self", values=name_list(apiAccessor.getAllSets()))
+        self.setList.pack()
+        self.setList.grid()
+        self.quitButton = tkinter.Button(self, text="Quit", command=self.quit)
+        self.quitButton.grid()
 
 class ApiAccessor:
 
@@ -23,4 +47,8 @@ for mtgSet in apiAccessor.getAllSets():
 
 print(len(apiAccessor.getAllCardsForSet("LEA").all()))
 
-DocumentWriter.createDocumentTest()
+# DocumentWriter.createDocumentTest(apiAccessor.getAllCardsForSet("KTK").all())
+
+app = Application()
+app.master.title("Set document creator.")
+app.mainloop()
--- DocumentWriter.py
+++ DocumentWriter.py
@@ -1,7 +1,17 @@
-from docx import *
-
-def createDocumentTest():
-    document = newdocument()
-    body = document.xpath('/w:document/w:body', namespaces=nsprefixes)[0]
-    body.append('Test text')
-    savedocx(document, None, None, None, None, None, 'Test.docx')
+def createDocumentTest(cards):
+    document = open(cards[0].set + ".txt", "w")
+    for card in cards:
+        cardText = ""
+        if card.name is not None:
+            cardText += card.name + "\n"
+        if card.mana_cost is not None:
+            cardText += card.mana_cost + "\n"
+        if card.power is not None:
+            cardText += card.power + "\\" + card.toughness + "\n"
+        if card.text is not None:
+            cardText += card.text + "\n"
+        if card.set is not None:
+            cardText += card.set + "-" + card.rarity + "\n"
+        cardText += "\n"
+        document.write(cardText)
+    document.close()
