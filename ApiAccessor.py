284ef712ac6ed4051c440591a3d592049c0fa996
thechucklingatom <thechucklingatom>
2018-06-26 20:39:19 -0600

    finalizing ui

--- ApiAccessor.py
+++ ApiAccessor.py
@@ -4,12 +4,30 @@ import DocumentWriter
 import tkinter
 import ttk
 
-def name_list(setList):
+
+class ApiAccessor:
+
+    @staticmethod
+    def get_all_sets():
+        return Set.all()
+
+    @staticmethod
+    def get_all_cards_for_set(name):
+        if name is '':
+            return [None]
+        return Card.where(set=name)
+
+
+apiAccessor = ApiAccessor()
+
+
+def name_list(set_list):
     return_list = []
-    for mtgSet in setList:
+    for mtgSet in set_list:
         return_list.append(mtgSet.name)
     return_list.sort()
     return return_list
+
 
 class Application(tkinter.Frame):
 
@@ -18,36 +36,18 @@ class Application(tkinter.Frame):
         tkinter.Frame.__init__(self, master, width=400, height=400)
         self.pack_propagate(0)
         self.grid()
-        self.createWidgets()
+        self.set_list = ttk.Combobox(self, textvariable="self", values=name_list(apiAccessor.get_all_sets()))
+        self.set_list.pack()
+        self.set_list.grid()
+        self.quit_button = tkinter.Button(self, text="Quit", command=self.quit)
+        self.quit_button.grid()
+        self.create_button = tkinter.Button(self, text="Create document", command=self.create_document())
+        self.create_button.grid()
 
-    def createWidgets(self):
-        self.setList = ttk.Combobox(self, textvariable="self", values=name_list(apiAccessor.getAllSets()))
-        self.setList.pack()
-        self.setList.grid()
-        self.quitButton = tkinter.Button(self, text="Quit", command=self.quit)
-        self.quitButton.grid()
+    def create_document(self):
+        api_accessor = ApiAccessor()
+        DocumentWriter.create_document_test(api_accessor.get_all_cards_for_set(self.set_list.get()).all())
 
-class ApiAccessor:
-
-    def getAllSets(self):
-        return Set.all()
-
-    def getAllCardsForSet(self, name):
-        return Card.where(set=name)
-
-
-def main():
-    print("test")
-
-
-apiAccessor = ApiAccessor()
-for mtgSet in apiAccessor.getAllSets():
-    print(mtgSet.code)
-
-
-print(len(apiAccessor.getAllCardsForSet("LEA").all()))
-
-# DocumentWriter.createDocumentTest(apiAccessor.getAllCardsForSet("KTK").all())
 
 app = Application()
 app.master.title("Set document creator.")
--- DocumentWriter.py
+++ DocumentWriter.py
@@ -1,17 +1,19 @@
-def createDocumentTest(cards):
+def create_document_test(cards):
+    if cards is None:
+        return
     document = open(cards[0].set + ".txt", "w")
     for card in cards:
-        cardText = ""
+        card_text = ""
         if card.name is not None:
-            cardText += card.name + "\n"
+            card_text += card.name + "\n"
         if card.mana_cost is not None:
-            cardText += card.mana_cost + "\n"
+            card_text += card.mana_cost + "\n"
         if card.power is not None:
-            cardText += card.power + "\\" + card.toughness + "\n"
+            card_text += card.power + "\\" + card.toughness + "\n"
         if card.text is not None:
-            cardText += card.text + "\n"
+            card_text += card.text + "\n"
         if card.set is not None:
-            cardText += card.set + "-" + card.rarity + "\n"
-        cardText += "\n"
-        document.write(cardText)
+            card_text += card.set + "-" + card.rarity + "\n"
+        card_text += "\n"
+        document.write(card_text)
     document.close()
